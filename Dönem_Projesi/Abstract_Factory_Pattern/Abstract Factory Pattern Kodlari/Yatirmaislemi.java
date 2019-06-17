b24af70a98e0269a8f48ee020ffc01104cf49043
Siham Amaouche <10710980+SihamAmarouche@users.noreply.github.com>
2019-06-17 12:40:19 +0300

    Delete Yatirmaislemi.java

--- Dönem_Projesi/Abstract_Factory_Pattern/Abstract Factory Pattern Kodlari/Yatirmaislemi.java
+++
@@ -1,9 +0,0 @@
-package AbstractFactory_Pattern;
-
-public class Yatirmaislemi extends Parasalislemi{
-	
-	public void islemTuru(){
-		System.out.println("Hesabýnýza para yatýrma iþlemi gerçekleþecektir!!");
-	}
-
-}
