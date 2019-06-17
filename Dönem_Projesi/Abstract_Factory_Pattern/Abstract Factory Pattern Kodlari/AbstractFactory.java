4d1f29b824ea60d3d2a21970ae5342665a4f2c28
Siham Amaouche <10710980+SihamAmarouche@users.noreply.github.com>
2019-06-17 12:39:19 +0300

    Delete AbstractFactory.java

--- Dönem_Projesi/Abstract_Factory_Pattern/Abstract Factory Pattern Kodlari/AbstractFactory.java
+++
@@ -1,8 +0,0 @@
-package AbstractFactory_Pattern;
-
-public abstract class AbstractFactory {
-	
-	abstract Kullanici getKullanici(String kullaniciTuru);
-	abstract Parasalislemi getParasalislemi(String islemTuru);
-
-}
